b3c0c7ca33152912feb80d8a4a84d26ff39d99b2
Juha-Matti Santero <juhasantero2@outlook.com>
2017-06-05 12:42:24 +0000

    Added search

--- Contoso.Events/Contoso.Events.Web/Controllers/HomeController.cs
+++ Contoso.Events/Contoso.Events.Web/Controllers/HomeController.cs
@@ -1,12 +1,18 @@
 ﻿using Contoso.Events.ViewModels;
 using System;
+using System.Configuration;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
 using System.Web.Mvc;
+using Contoso.Events.Models;
+using Newtonsoft.Json;
 
 namespace Contoso.Events.Web.Controllers
 {
     public class HomeController : Controller
     {
-        private const int EVENTS_GRID_SIZE = 10;
+        private static readonly HttpClient Client = new HttpClient();
 
         public ActionResult Index()
         {
@@ -15,11 +21,30 @@ namespace Contoso.Events.Web.Controllers
             return View(viewModel);
         }
 
-        public ActionResult Search()
-        { 
-            EventsViewModel viewModel = new EventsViewModel();
+        [HttpGet]
+        public async Task<ActionResult> Search(SearchViewModel model)
+        {
+            string searchTerm = model.Term;
+            if (string.IsNullOrWhiteSpace(searchTerm))
+            {
+                searchTerm = "*";
+            }
+            string url = $"https://aa-events-prod-search.search.windows.net/indexes/aa-events-index/docs?search={searchTerm}&api-version=2016-09-01";
 
-            return View(viewModel);
+            var req = new HttpRequestMessage(HttpMethod.Get, url);
+            string accessKey = ConfigurationManager.AppSettings["SearchAccessKey"];
+            req.Headers.Add("api-key", accessKey);
+            req.Headers.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+            HttpResponseMessage res = await Client.SendAsync(req);
+
+            string json = await res.Content.ReadAsStringAsync();
+            var result = JsonConvert.DeserializeObject<AzureSearchResult<EventSearchResult>>(json);
+
+            model.Term = searchTerm;
+            model.SearchResults = result.Items;
+
+            return View(model);
         }
 
         public ActionResult Event(string id)
@@ -70,7 +95,7 @@ namespace Contoso.Events.Web.Controllers
                     return View("Registered", viewModel.Registration.Id);
                 }
             }
-            
+
             return View(viewModel);
         }
     }
